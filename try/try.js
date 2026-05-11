911e9f99bbcb22e1ae8e0e581d65a6776db63a1a
marcdoodleys <marcdoodleys@gmail.com>
2026-05-11 01:21:58 -0400

    commit

---
+++ try/try.js
@@ -0,0 +1,23 @@
+// Ceci est un commentaire
+const school="BYU-I";
+let studentName ="Marc";
+let age="21";
+let isStudent= true;
+
+console.log(studentName);
+console.log(age)
+
+age=22;
+console.log(age);
+
+//array
+let fruits=["apple", "banana", "orange"];
+console.log(fruits[1]);
+
+//Object
+let dog = {
+    name:"Spot",
+    breed: "Dalmatian"
+};
+
+console.log(dog.name);
