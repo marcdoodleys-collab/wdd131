32f784e4c4a12b0b9e4539f6b2ae6c1c063a1ecd
marcdoodleys <marcdoodleys@gmail.com>
2026-05-19 16:39:02 -0400

    commit

--- w02/scripts/bom.js
+++ w02/scripts/bom.js
@@ -1,48 +1,82 @@
+// ================================
 // Sélection des éléments du DOM
-const input = document.querySelector('#favchap');
-const button = document.querySelector('button');
-const list = document.querySelector('#list');
-
+// ================================
+ 
+// Partie liste
+const input      = document.querySelector('#favchap');
+const addButton  = document.querySelector('#addButton');
+const list       = document.querySelector('#list');
+ 
+// Partie menu hamburger
+const hamButton  = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+ 
+ 
+// ================================
 // Événement du bouton Ajouter
-button.addEventListener('click', function () {
-
+// ================================
+ 
+addButton.addEventListener('click', function () {
+ 
     // Vérifie que le champ n'est pas vide
     if (input.value.trim() !== '') {
-
+ 
         // Création des éléments
-        const li = document.createElement('li');
+        const li           = document.createElement('li');
         const deleteButton = document.createElement('button');
-
-        // Ajout du texte
+ 
+        // Ajout du texte dans le li
         li.textContent = input.value;
-
+ 
         // Texte du bouton supprimer
         deleteButton.textContent = '❌';
-
+ 
         // Accessibilité
         deleteButton.setAttribute('aria-label', `Remove ${input.value}`);
-
+ 
         // Ajouter le bouton dans le li
         li.append(deleteButton);
-
+ 
         // Ajouter le li dans la liste
         list.append(li);
-
+ 
+        // ================================
         // Événement supprimer
+        // ================================
+ 
         deleteButton.addEventListener('click', function () {
+ 
+            // Supprime le li
             list.removeChild(li);
+ 
+            // Remet le focus dans le champ
             input.focus();
         });
-
+ 
         // Nettoyer le champ texte
         input.value = '';
-
+ 
         // Remettre le curseur dans le champ
         input.focus();
-
+ 
     } else {
-
-        // Si vide → remettre le focus
+ 
+        // Si le champ est vide, remettre le focus
         input.focus();
     }
 });
+ 
+ 
+// ================================
+// Menu Hamburger
+// ================================
+ 
+hamButton.addEventListener('click', () => {
+ 
+    // Ouvre / Ferme le menu
+    navigation.classList.toggle('open');
+ 
+    // Anime le bouton hamburger
+    hamButton.classList.toggle('open');
+});
+ 
